49156360cd254f2ec7ce145d713df587d2970e5c
Ivan Torcuato <2137332@slu.edu.ph>
2018-05-07 21:52:13 +0800

    modification of some parts

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 1. Install node
 2. `npm i`
-3. `npm start`
-
+3. `npm start` or `npm run dev`
--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const Sequelize = require('sequelize');
+const bodyParser = require('body-parser');
 const db = require('./db');
-
-const $ = require('jquery');
 
 const app = express();
 
@@ -12,6 +11,8 @@ app.engine('handlebars', exphbs({ defaultLayout: 'default' }));
 app.set('view engine', 'handlebars');
 
 app.use('/static', express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('login');
@@ -22,21 +23,41 @@ app.get('/coursewebsite', (req, res) => {
 })
 // Route to get questions
 app.get('/questions', (req, res) => {
-  db.query('SELECT * FROM questions', { type: Sequelize.QueryTypes.SELECT })
-    .then(questions => {
-      db.query('SELECT * FROM options', { type: Sequelize.QueryTypes.SELECT })
-      .then(options => {
-        res.render('index', { questions, options });
-      })
-    })
-});
-
-app.get('/test', (req, res) => {
   db.query('SELECT * FROM questions JOIN options ON id == question_id ORDER BY id ASC, code ASC', {type: Sequelize.QueryTypes.SELECT})
     .then(questions => {
-      res.render('test', { questions });
-    })
-})
+      var questionArr = [];
+      for(var i = 0; i < questions.length; i+=3) {
+        questionArr.push({
+          "id": questions[i].id,
+          "content": questions[i].content,
+          "options": {
+            "option1": {
+              "code": questions[i].code,
+              "option": questions[i].option
+            },
+            "option2": {
+              "code": questions[i+1].code,
+              "option": questions[i+1].option
+            },
+            "option3": {
+              "code": questions[i+2].code,
+              "option": questions[i+2].option
+            }
+          }
+        });
+      }
+      res.render('questions', { questionArr });
+    });
+});
+
+app.get('/sample', (req, res) => {
+  res.render('sample');
+});
+
+app.get('/greetme', (req, res) => {
+  var sample = req.query.username;
+  res.render('greatme', {sample} );
+});
 
 
 
